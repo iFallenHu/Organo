d6b0411b94bcfa695a458c21ac2a844ba78519a9
Luan Reis <luannascimento.microcamp@gmail.com>
2025-07-08 22:56:50 -0300

    criando componente cabeçalho

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabecalhoComponent
   ],
   imports: [
     BrowserModule,
